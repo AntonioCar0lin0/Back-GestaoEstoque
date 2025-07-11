5d2a30641bb1ac3fddb1a862ddd585f7ebc17210
anton <antoniorpcarolino@gmail.com>
2025-07-11 18:27:02 -0300

    fix: adicionando as dependências corretas

--- src/app.js
+++ src/app.js
@@ -39,7 +39,7 @@ app.use('/users', userRoutes);
 app.use('/products', productsRoutes)
 
 // iniciar o servidor
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
