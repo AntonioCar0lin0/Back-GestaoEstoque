e996b2f126477fe0658daa9bbe6cfd1952d1ae98
Antonio Rodrigues Pinheiro Carolino <antonio.rodrigues@pe.sebrae.com.br>
2025-07-18 10:43:06 -0300

    fix: bugs in dashboard controller

--- src/models/Categoria.js
+++ src/models/Categoria.js
@@ -1,7 +1,6 @@
 const { DataTypes } = require('sequelize');
 const sequelize     = require('../config/database');
 
-
 const Categoria = sequelize.define('Categoria', {
   id: {
     type: DataTypes.INTEGER,
@@ -13,6 +12,17 @@ const Categoria = sequelize.define('Categoria', {
     allowNull: false,
     unique: true,
   },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  color: {
+    type: DataTypes.STRING(7), // ex: "#FF0000"
+    allowNull: true,
+    validate: {
+      is: /^#([0-9A-F]{3}){1,2}$/i
+    }
+  }
 }, {
   tableName:  'categorias',
   timestamps: true,
